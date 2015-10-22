00359e2434b7541eda299e4a3d31c6ed4ddeca21
mstg <mustiigezen@gmail.com>
2015-10-22 11:35:23 +0200

    hey

---
+++ asb.py
@@ -0,0 +1,19 @@
+import wmi, serial, time
+
+if __name__ == "__main__":
+    ser = serial.Serial("COM5")
+    currentVal = 40
+    c = wmi.WMI(namespace='wmi')
+
+    methods = c.WmiMonitorBrightnessMethods()[0]
+    while True:
+        val = ser.readline().strip()
+
+        should_continue = True
+
+        if val == currentVal:
+            should_continue = False
+
+        if should_continue == True:
+            currentVal = val
+            methods.WmiSetBrightness(currentVal, 0)
